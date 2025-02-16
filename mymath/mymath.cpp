d093ef9bd0a1d312416fc81776bc67bee611d77a
Alexey-creator875 <lexanav1245@mail.ru>
2025-02-16 21:40:48 +0300

    add static library mymath

---
+++ mymath/mymath.cpp
@@ -0,0 +1,11 @@
+#include "mymath.h"
+
+namespace mymath {
+[[nodiscard]] int sum(int a, int b) {
+    return a + b;
+}
+
+[[nodiscard]] int multiply(int a, int b) {
+    return a * b;
+}
+}  // namespace mymath
---
+++ mymath/mymath.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace mymath {
+[[nodiscard]] int sum(int a, int b);
+[[nodiscard]] int multiply(int a, int b);
+}  // namespace mymath
