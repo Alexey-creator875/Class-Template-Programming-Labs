fd48358eda241857e99d56a77abd5411645e83c7
Alexey-creator875 <lexanav1245@mail.ru>
2025-03-18 20:30:20 +0300

    add working files

---
+++ Fraction/Fraction.cpp
@@ -0,0 +1,125 @@
+#include "Fraction.h"
+
+#include <cmath>
+#include <cstddef>
+#include <cstring>
+#include <iostream>
+
+namespace {
+const int kDefaultNumeratorValue = 0;
+const int kDefaultDenominatorValue = 1;
+const int kPrecision = 4;
+const int kDecimalBase = 10;
+
+const size_t kBufferSize = 50;
+
+const int kNextSymbol = 1;
+
+int gcd(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+
+    return gcd(b, a % b);
+}
+
+}  // namespace
+
+void Fraction::reduce() {
+    int greatestCommonDivisor = gcd(std::abs(numerator), std::abs(denominator));
+    numerator /= greatestCommonDivisor;
+    denominator /= greatestCommonDivisor;
+}
+
+Fraction::Fraction() {
+    numerator = kDefaultNumeratorValue;
+    denominator = kDefaultDenominatorValue;
+}
+
+Fraction::Fraction(int numerator, int denominator) {
+    if (denominator == 0) {
+        throw std::runtime_error("division by zero");
+    }
+
+    this->numerator = ((denominator < 0) ? -numerator : numerator);
+    this->denominator = std::abs(denominator);
+    this->reduce();
+}
+
+Fraction::Fraction(double number) {
+    numerator = static_cast<int>(number * std::pow(kDecimalBase, kPrecision));
+    denominator = std::pow(kDecimalBase, kPrecision);
+    this->reduce();
+}
+
+Fraction::Fraction(const char* string) : Fraction() {
+    if (std::strchr(string, ' ') && std::strchr(string, '/')) {
+        int integerPart = std::stoi(string);
+        denominator = std::stoi(std::strchr(string, '/') + kNextSymbol);
+        numerator = (std::abs(integerPart) * denominator + std::stoi(std::strchr(string, ' ') + kNextSymbol)) * ((integerPart < 0) ? -1 : 1);
+    }
+
+    if (!std::strchr(string, ' ') && std::strchr(string, '/')) {
+        numerator = std::stoi(string);
+        denominator = std::stoi(std::strchr(string, '/') + kNextSymbol);
+    }
+
+    if (!std::strchr(string, '/')) {
+        numerator = std::stoi(string);
+    }
+
+    this->reduce();
+}
+
+void Fraction::setNumerator(int numerator) {
+    this->numerator = numerator;
+}
+
+void Fraction::setDenominator(int denominator) {
+    this->denominator = denominator;
+}
+
+int Fraction::getNumerator() {
+    return numerator;
+}
+
+int Fraction::getDenominator() {
+    return denominator;
+}
+
+std::ostream& operator<<(std::ostream& out, const Fraction& object) {
+    if (object.numerator % object.denominator == 0) {
+        out << object.numerator;
+        return out;
+    }
+
+    if (object.numerator / object.denominator == 0) {
+        out << object.numerator << '/' << object.denominator;
+        return out;
+    }
+
+    out << object.numerator / object.denominator << ' ' << std::abs(object.numerator % object.denominator) << '/' << object.denominator;
+    return out;
+}
+
+std::istream& operator>>(std::istream& in, Fraction& object) {
+    char buffer[kBufferSize]{};
+    in.getline(buffer, kBufferSize, '\n');
+    object = buffer;
+    return in;
+}
+
+Fraction Fraction::operator+(const Fraction& second) {
+    return Fraction(numerator * second.denominator + second.numerator * denominator, denominator * second.denominator);
+}
+
+Fraction& Fraction::operator+=(const Fraction& second) {
+    numerator = numerator * second.denominator + second.numerator * denominator;
+    denominator *= second.denominator;
+    this->reduce();
+    return *this;
+}
+
+Fraction operator+(const double first, const Fraction& second) {
+    return Fraction(first) + second;
+}
---
+++ Fraction/Fraction.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+
+class Fraction {
+ private:
+    int numerator;
+    int denominator;
+
+    void reduce();
+
+ public:
+    Fraction();
+    Fraction(int numerator, int denominator);
+    Fraction(double number);
+    Fraction(const char* string);
+
+    void setNumerator(int numerator);
+    void setDenominator(int denominator);
+    int getNumerator();
+    int getDenominator();
+
+    friend std::ostream& operator<<(std::ostream& out, const Fraction& object);
+    friend std::istream& operator>>(std::istream& in, Fraction& object);
+
+    Fraction operator+(const Fraction& second);
+    Fraction& operator+=(const Fraction& second);
+    friend Fraction operator+(const double first, const Fraction& second);
+};
