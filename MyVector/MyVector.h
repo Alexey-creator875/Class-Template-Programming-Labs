d69997e537e21154f2bd9e88daa03c59ede0084f
Alexey-creator875 <lexanav1245@mail.ru>
2025-05-12 18:39:06 +0300

    optimise

--- MyVector/MyVector.h
+++ MyVector/MyVector.h
@@ -13,6 +13,19 @@ const size_t kReductionFactor = 4;
 }  // namespace
 
 template<class T>
+int Compare(const T first, const T second) {
+    if (first == second) {
+        return 0;
+    }
+
+    if (first < second) {
+        return -1;
+    }
+
+    return 1;
+}
+
+template<class T>
 class MyVector {
  protected:
     size_t size;
@@ -56,6 +69,7 @@ void MyVector<T>::resize() {
 
     T* buffer = vector;
     vector = new T[capacity];
+
     std::copy(buffer, buffer + size, vector);
     delete[] buffer;
     return;
@@ -73,37 +87,17 @@ MyVector<T>::MyVector(const T element) {
     capacity = kDefaultVectorCapacity;
     size = 0;
     vector = new T[kDefaultVectorCapacity];
-    *vector = element;
-    ++size;
-}
-
-template<>
-inline MyVector<char*>::MyVector(char* string) {
-    capacity = kDefaultVectorCapacity;
-    size = 0;
-    vector = new char*[kDefaultVectorCapacity];
-    *vector = new char[std::strlen(string) + 1];
-    std::strcpy(*vector, string);
-    ++size;
+    pushBack(element);
 }
 
 template<class T>
 MyVector<T>::MyVector(const MyVector<T>& object) {
     capacity = object.capacity;
-    size = object.size;
-    vector = new T[capacity];
-    std::copy(object.vector, object.vector + object.size, vector);
-}
+    size = 0;
+    vector = new T[object.capacity];
 
-template<>
-inline MyVector<char*>::MyVector(const MyVector<char*>& object) {
-    capacity = object.capacity;
-    size = object.size;
-    vector = new char*[capacity];
-
-    for (size_t i = 0; i < size; ++i) {
-        vector[i] = new char[std::strlen(object[i]) + 1];
-        std::strcpy(vector[i], object[i]);
+    for (size_t i = 0; i < object.size; ++i) {
+        pushBack(object[i]);
     }
 }
 
@@ -112,20 +106,9 @@ MyVector<T>::~MyVector() {
     delete[] vector;
 }
 
-template<>
-inline MyVector<char*>::~MyVector() {
-    for (size_t i = 0; i < capacity; ++i) {
-        if (vector[i]) {
-            delete[] vector[i];
-        }
-    }
-
-    delete[] vector;
-}
-
 template<class T>
 int MyVector<T>::length() const {
-    return size;
+    return static_cast<int>(size);
 }
 
 template<class T>
@@ -135,21 +118,6 @@ void MyVector<T>::pushBack(const T element) {
     }
 
     vector[size++] = element;
-}
-
-template<>
-void inline MyVector<char*>::pushBack(char* string) {
-    if (size >= capacity) {
-        resize();
-    }
-
-    if (vector[size]) {
-        delete[] vector[size];
-    }
-
-    vector[size] = new char[std::strlen(string) + 1];
-
-    std::strcpy(vector[size++], string);
 }
 
 template<class T>
@@ -169,40 +137,10 @@ void MyVector<T>::deleteElement(const int index) {
     }
 }
 
-template<>
-void inline MyVector<char*>::deleteElement(const int index) {
-    if (index < 0 || index >= size) {
-        throw std::runtime_error("index out of boundary");
-    }
-
-    --size;
-
-    for (size_t i = static_cast<size_t>(index); i < size; ++i) {
-        delete[] vector[i];
-        vector[i] = new char[std::strlen(vector[i + 1]) + 1];
-        std::strcpy(vector[i], vector[i + 1]);
-    }
-
-    if (capacity > kDefaultVectorCapacity && size < (capacity / kReductionFactor)) {
-        resize();
-    }
-}
-
 template<class T>
 int MyVector<T>::findElement(const T element) {
     for (size_t i = 0; i < size; ++i) {
-        if (vector[i] == element) {
-            return static_cast<int>(i);
-        }
-    }
-
-    return -1;
-}
-
-template<>
-int inline MyVector<char*>::findElement(char* string) {
-    for (size_t i = 0; i < size; ++i) {
-        if (!std::strcmp(vector[i], string)) {
+        if (!Compare(vector[i], element)) {
             return static_cast<int>(i);
         }
     }
@@ -218,23 +156,7 @@ void MyVector<T>::sort(bool ascending) {
         sorted = true;
 
         for (size_t j = 0; j < size - i - 1; ++j) {
-            if ((ascending) ? vector[j + 1] < vector[j] : vector[j] < vector[j + 1]) {
-                std::swap(vector[j], vector[j + 1]);
-                sorted = false;
-            }
-        }
-    }
-}
-
-template<>
-void inline MyVector<char*>::sort(bool ascending) {
-    bool sorted = false;
-
-    for (size_t i = 0; i < size && !sorted; ++i) {
-        sorted = true;
-
-        for (size_t j = 0; j < size - i - 1; ++j) {
-            if ((ascending) ? std::strcmp(vector[j], vector[j + 1]) > 0 : std::strcmp(vector[j], vector[j + 1]) < 0) {
+            if (ascending ? Compare(vector[j], vector[j + 1]) > 0 : Compare(vector[j], vector[j + 1]) < 0) {
                 std::swap(vector[j], vector[j + 1]);
                 sorted = false;
             }
@@ -267,27 +189,11 @@ MyVector<T>& MyVector<T>::operator=(const MyVector<T>& object) {
     }
 
     capacity = object.capacity;
-    size = object.size;
+    size = 0;
     vector = new T[object.capacity];
 
-    std::copy(object.vector, object.vector + object.size, vector);
-
-    return *this;
-}
-
-template<>
-inline MyVector<char*>& MyVector<char*>::operator=(const MyVector<char*>& object) {
-    if (vector) {
-        delete[] vector;
-    }
-
-    capacity = object.capacity;
-    size = object.size;
-    vector = new char*[object.capacity];
-
-    for (size_t i = 0; i < size; ++i) {
-        vector[i] = new char[std::strlen(object[i]) + 1];
-        std::strcpy(vector[i], object[i]);
+    for (size_t i = 0; i < object.size; ++i) {
+        pushBack(object[i]);
     }
 
     return *this;
